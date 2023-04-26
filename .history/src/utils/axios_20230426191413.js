88801411381f99a9762fc2629187c9bd90f9c4de
STATICHIT <108850075+STATICHIT@users.noreply.github.com>
2023-04-26 19:25:38 +0800

    Delete .history directory

--- .history/src/utils/axios_20230426191413.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-import qs from 'qs';//转json数据工具包
-import { ElMessage } from 'element-plus'
-import router from '../router/index'
-import { localGet } from './index'
-import user from "../store/user";
-const userStore = user()
