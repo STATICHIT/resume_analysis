1a2fb98b0249eb4e5282dd61baa92764e6842216
STATICHIT <2394412110@qq.com>
2023-05-02 11:47:35 +0800

    .gitignore文件中不要增加任何注释，否则会漏掉文件

---
+++ src/views/Main.vue
@@ -0,0 +1,62 @@
+<!--
+ * @Author: STATICHIT
+ * @Date: 2023-05-02 11:33:17
+ * @LastEditors: STATICHIT 2394412110@qq.com
+ * @LastEditTime: 2023-05-02 11:40:18
+ * @FilePath: \resume_analysis\src\views\Main.vue
+ * @Description: 客户端布局底页
+-->
+<template>
+  <div class="common-layout">
+    <el-container>
+        <div class="top">
+          <Top></Top>
+        </div>
+      <el-main class="body">
+        <router-view></router-view>
+      </el-main>
+      <el-footer>
+        <div>
+          <Foot></Foot>
+        </div>
+      </el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Top from "./main/Top.vue"
+import Body from "./main/Body.vue"
+import Foot from "./main/Foot.vue"
+export default({
+  components: {
+    Top,
+    Body,
+    Foot,
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.el-header{
+  --el-header-padding:0px;
+}
+.top{
+  position:fixed;
+  top:0;
+  height: 50px;
+  width: 100%;
+  z-index: 1000;
+}
+.body{
+margin-top: 40px;
+}
+</style>
---
+++ src/views/page/Message.vue
@@ -0,0 +1,172 @@
+<!--
+ * @Author: STATICHIT
+ * @Date: 2023-05-02 10:48:07
+ * @LastEditors: STATICHIT 2394412110@qq.com
+ * @LastEditTime: 2023-05-02 10:59:05
+ * @FilePath: \resume_analysis\src\views\page\Message.vue
+ * @Description: 消息接收页面
+-->
+<template>
+  <div class="box">
+    <div class="container">
+      <el-tabs v-model="message">
+        <el-tab-pane :label="`未读消息(${state.unread.length})`" name="first">
+          <el-table
+            :data="state.unread"
+            :show-header="false"
+            style="width: 100%"
+          >
+            <el-table-column>
+              <template #default="scope">
+                <span class="message-title">{{ scope.row.title }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column prop="date" width="180"></el-table-column>
+            <el-table-column width="120">
+              <template #default="scope">
+                <el-button size="small" @click="handleRead(scope.$index)"
+                  >标为已读</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="handle-row">
+            <el-button type="primary">全部标为已读</el-button>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane :label="`已读消息(${state.read.length})`" name="second">
+          <template v-if="message === 'second'">
+            <el-table
+              :data="state.read"
+              :show-header="false"
+              style="width: 100%"
+            >
+              <el-table-column>
+                <template #default="scope">
+                  <span class="message-title">{{ scope.row.title }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column prop="date" width="150"></el-table-column>
+              <el-table-column width="120">
+                <template #default="scope">
+                  <el-button type="danger" @click="handleDel(scope.$index)"
+                    >删除</el-button
+                  >
+                </template>
+              </el-table-column>
+            </el-table>
+            <div class="handle-row">
+              <el-button type="danger">删除全部</el-button>
+            </div>
+          </template>
+        </el-tab-pane>
+        <el-tab-pane :label="`回收站(${state.recycle.length})`" name="third">
+          <template v-if="message === 'third'">
+            <el-table
+              :data="state.recycle"
+              :show-header="false"
+              style="width: 100%"
+            >
+              <el-table-column>
+                <template #default="scope">
+                  <span class="message-title">{{ scope.row.title }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column prop="date" width="150"></el-table-column>
+              <el-table-column width="120">
+                <template #default="scope">
+                  <el-button @click="handleRestore(scope.$index)"
+                    >还原</el-button
+                  >
+                </template>
+              </el-table-column>
+            </el-table>
+            <div class="handle-row">
+              <el-button type="danger">清空回收站</el-button>
+            </div>
+          </template>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+export default {
+  name: "tabs",
+  setup() {
+    const message = ref("first");
+    const state = reactive({
+      unread: [
+        {
+          date: "2022-12-26 20:00:00",
+          title:
+            "您在2022-12-26 19:59:01上传的100条简历已录入并进行简历分析成功，您可以在人才库中查看详细内容",
+        },
+        {
+          date: "2022-12-25 20:30:00",
+          title: "新功能【人岗匹配】已上线，快去试试吧~",
+        },
+        {
+          date: "2022-12-25 20:00:00",
+          title: "新功能【自动化管理】已上线，快去试试吧~",
+        },
+      ],
+      read: [
+        {
+          date: "2022-12-19 20:00:00",
+          title: "系统于今晚进行不停服更新，若对您造成了不便，敬请谅解",
+        },
+      ],
+      recycle: [
+        {
+          date: "2022-12-26 20:00:00",
+          title: "今日更新五条岗位，请在岗位库中查看详细",
+        },
+      ],
+    });
+
+    const handleRead = (index) => {
+      const item = state.unread.splice(index, 1);
+      console.log(item);
+      state.read = item.concat(state.read);
+    };
+    const handleDel = (index) => {
+      const item = state.read.splice(index, 1);
+      state.recycle = item.concat(state.recycle);
+    };
+    const handleRestore = (index) => {
+      const item = state.recycle.splice(index, 1);
+      state.read = item.concat(state.read);
+    };
+
+    return {
+      message,
+      state,
+      handleRead,
+      handleDel,
+      handleRestore,
+    };
+  },
+};
+</script>
+
+<style>
+.box {
+  background-color: rgba(227, 231, 231, 0.37);
+  text-align: left;
+  height: 800px;
+  width: 1000px;
+  margin: auto;
+  padding: 40px;
+  border-radius: 30px;
+}
+.message-title {
+  cursor: pointer;
+}
+.handle-row {
+  margin-top: 30px;
+}
+</style>
+
