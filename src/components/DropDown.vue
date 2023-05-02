be3c6519ae2e15b1c8877df27f379c179998a059
STATICHIT <2394412110@qq.com>
2023-05-02 10:40:54 +0800

    DropDown组件

---
+++ src/components/DropDown.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-dropdown>
+    <span class="area">
+      {{ name }}
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>
+          <img style="object-fit: scale-down" :src="idView" />
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+<script>
+export default {
+  name: "dropDown",
+  data() {
+    return {
+      name: "我是一个悬浮下拉框",
+      idView: require("../assets/01.jpg"),
+    };
+  },
+  methods: {},
+};
+</script>
+<style>
+.area{
+  margin-top: 19px;
+  font-size:13px;
+  color: #cfcfcf;
+}
+</style>
