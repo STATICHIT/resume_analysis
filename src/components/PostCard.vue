a4a54eb2864d7949d90d0f31f9c49a78fba50132
STATICHIT <2394412110@qq.com>
2023-07-06 15:20:37 +0800

    解决岗位卡片排列样式出错问题

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -2,7 +2,7 @@
  * @Author: STATICHIT
  * @Date: 2023-05-24 21:10:51
  * @LastEditors: STATICHIT 2394412110@qq.com
- * @LastEditTime: 2023-07-03 10:22:41
+ * @LastEditTime: 2023-07-06 11:16:25
  * @FilePath: \resume_analysis\src\components\PostCard.vue
  * @Description: 岗位展示列表
 -->
@@ -12,9 +12,20 @@
       <h2 style="text-align: center">现有岗位</h2>
       <p style="text-align: center">Current positions</p>
     </div>
-    <br><br>
+    <br /><br />
     <div class="container">
-      <div class="col">
+      <div v-for="(job, idx) in list" :key="idx" class="card-container">
+        <div>
+          <div class="overlay"></div>
+          <div class="card">
+            <h4>{{ job.name }}</h4>
+            <h5>{{ job.area }}</h5>
+            <span class="chev">&rsaquo;</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="container">
         <div class="card-container">
           <div class="overlay"></div>
           <div class="overlay"></div>
@@ -50,9 +61,6 @@
             <span class="chev">&rsaquo;</span>
           </div>
         </div>
-      </div>
-
-      <div class="col">
         <div class="card-container">
           <div class="overlay"></div>
           <div class="overlay"></div>
@@ -88,9 +96,6 @@
             <span class="chev">&rsaquo;</span>
           </div>
         </div>
-      </div>
-
-      <div class="col">
         <div class="card-container">
           <div class="overlay"></div>
           <div class="overlay"></div>
@@ -126,8 +131,7 @@
             <span class="chev">&rsaquo;</span>
           </div>
         </div>
-      </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -173,81 +177,30 @@ let list = [
 </script>
 
 <style lang="scss" scoped>
-@import url("https://fonts.googleapis.com/css?family=Fira+Sans:400");
-
-*,
-*:before,
-*:after {
+* {
   box-sizing: border-box;
 }
 
-* {
-  user-select: none;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  transform-style: preserve-3d;
-  margin: 0;
-  padding: 0;
-}
-
-*:focus {
-  outline: none !important;
-}
 .box {
   padding: 40px 100px;
 }
-body {
-  margin: 0;
-  padding: 0;
-  background: #131313;
-  color: #f5f5f5;
-  font-family: "Fira Sans";
-  text-align: left;
-  overflow-x: hidden;
-  overflow-y: hidden;
-}
-
 .container {
   width: 100%;
   height: 100%;
   display: flex;
-  flex-flow: row;
+  flex-wrap: wrap;
+  //flex-flow: row;
   perspective: 1000px;
   perspective-origin: center;
 }
 
-.col {
-  height: 600px;
-  transition: transform 600ms cubic-bezier(0.59, 0.875, 0.865, 1),
-    opacity 300ms ease;
-  transform: translateZ(34px) scale(0.98);
-  flex: 0 0 33.3333%;
-  position: relative;
-}
-
-.col:nth-of-type(1) {
-  transform: rotateY(5deg) translateZ(34px) scale(0.98);
-}
-
-.col:nth-of-type(3) {
-  transform: rotateY(-5deg) translateZ(34px) scale(0.98);
-}
-
-.col:hover,
-.col:focus {
-  transform: translateZ(34px);
-  transition: transform 300ms cubic-bezier(0.39, 0.575, 0.565, 1),
-    opacity 300ms ease;
-}
-
 .card-container {
   position: relative;
-  width: calc(100% - 25px);
-  height: calc(33.3% - 25px);
+  width: 31%;
+  height: 160px;
   margin: 17px;
-  padding: 8px;
   text-align: center;
 }
-
 .card-container:hover,
 .card-container:focus {
   opacity: 1;
@@ -262,54 +215,13 @@ body {
   z-index: 1;
   transform: translateZ(34px);
 }
-
 .overlay:nth-of-type(1) {
   left: 0;
   top: 0;
 }
-
-.overlay:nth-of-type(2) {
-  right: 0;
-  top: 0;
-}
-
-.overlay:nth-of-type(3) {
-  bottom: 0;
-  right: 0;
-}
-
-.overlay:nth-of-type(4) {
-  bottom: 0;
-  left: 0;
-}
-
-.overlay:nth-of-type(1):hover ~ .card,
-.overlay:nth-of-type(1):focus ~ .card {
-  transform-origin: right top;
-  transform: rotateX(3deg) rotateY(-3deg) translateZ(0);
-}
-
-.overlay:nth-of-type(2):hover ~ .card,
-.overlay:nth-of-type(2):focus ~ .card {
-  transform-origin: left top;
-  transform: rotateX(3deg) rotateY(3deg) translateZ(0);
-}
-
-.overlay:nth-of-type(3):hover ~ .card,
-.overlay:nth-of-type(3):focus ~ .card {
-  transform-origin: left bottom;
-  transform: rotateX(-3deg) rotateY(3deg) translateZ(0);
-}
-
-.overlay:nth-of-type(4):hover ~ .card,
-.overlay:nth-of-type(4):focus ~ .card {
-  transform-origin: right bottom;
-  transform: rotateX(-3deg) rotateY(-3deg) translateZ(0);
-}
-
 .card {
   border-radius: 5px;
-  height: 100%;
+  height: 160px;
   transition: all 300ms ease-out;
   align-items: center;
   display: flex;
@@ -349,39 +261,39 @@ body {
   transform: scale(1);
 }
 
-.col:nth-of-type(1) .card-container:nth-of-type(1) .card:before {
+.card-container:nth-of-type(1) .card:before {
   background-image: url("https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/pia22474-2000.jpg?itok=cajl1lYH");
 }
 
-.col:nth-of-type(2) .card-container:nth-of-type(1) .card:before {
+.card-container:nth-of-type(2) .card:before {
   background-image: url("https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/m18-090.jpg?itok=TQ8Fuk4x");
 }
 
-.col:nth-of-type(3) .card-container:nth-of-type(1) .card:before {
+.card-container:nth-of-type(3) .card:before {
   background-image: url("https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/40808879360_18c96d62f1_o.jpg?itok=NttmTWPK");
 }
 
-.col:nth-of-type(1) .card-container:nth-of-type(2) .card:before {
+.card-container:nth-of-type(4) .card:before {
   background-image: url("https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/jwst-pv-145604copy.jpg?itok=Jq7GN-Rc");
 }
 
-.col:nth-of-type(2) .card-container:nth-of-type(2) .card:before {
+.card-container:nth-of-type(5) .card:before {
   background-image: url("https://www.nasa.gov/sites/default/files/styles/ubernode_alt_horiz/public/thumbnails/image/worldview201709201920x1080.png");
 }
 
-.col:nth-of-type(3) .card-container:nth-of-type(2) .card:before {
+.card-container:nth-of-type(6) .card:before {
   background-image: url("https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/nasasibexobs.jpg?itok=U2LCp14i");
 }
 
-.col:nth-of-type(1) .card-container:nth-of-type(3) .card:before {
+.card-container:nth-of-type(7) .card:before {
   background-image: url("https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/pia19808.jpg?itok=rgxgamMM");
 }
 
-.col:nth-of-type(2) .card-container:nth-of-type(3) .card:before {
+.card-container:nth-of-type(8) .card:before {
   background-image: url("https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/e0102_lg.jpg?itok=sd9lOayI");
 }
 
-.col:nth-of-type(3) .card-container:nth-of-type(3) .card:before {
+.card-container:nth-of-type(9) .card:before {
   background-image: url("https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/asteroid20180605-home.jpg?itok=KSUffiCR");
 }
 
@@ -427,55 +339,5 @@ h4 {
   opacity: 1;
   transform: translateX(3px);
   animation: slideRight 300ms ease;
-}
-
-@media (max-width: 768px) {
-  body {
-    overflow-y: scroll;
-  }
-  h4 {
-    font-size: 17px;
-  }
-}
-
-@media (max-width: 540px) {
-  .container {
-    flex-flow: column;
-    perspective: none;
-  }
-
-  .col:nth-of-type(1),
-  .col:nth-of-type(2),
-  .col:nth-of-type(3),
-  .col:hover {
-    transform: none;
-  }
-
-  h4 {
-    padding-bottom: 8px;
-    font-size: 18px;
-  }
-
-  .card > span {
-    display: none;
-  }
-}
-
-@media (max-height: 599px) {
-  .card > span {
-    display: none;
-  }
-}
-
-@media (max-width: 860px) and (max-height: 540px) {
-  h4 {
-    font-size: 14px;
-  }
-}
-
-@media (max-width: 620px) and (max-height: 540px) {
-  h4 {
-    font-size: 13px;
-  }
 }
 </style>
